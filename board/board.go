6f01ea473d6f5a740fc393026ce89c2a214deb42
James Vongphasouk <james.vongpha@gmail.com>
2025-09-04 13:22:40 -0600

    feat: attack masks of pawns

--- board/board.go
+++ board/board.go
@@ -22,81 +22,81 @@ type cBoard struct {
 	blackKing    uint64
 }
 
-// enum for board squares
+// enum for board squares in LERF (Little-Endian Rank-File) mapping opposite order
 type BitSquare int
 
 const (
-	A8 BitSquare = iota
-	B8
-	C8
-	D8
-	E8
-	F8
-	G8
-	H8
-	A7
-	B7
-	C7
-	D7
-	E7
-	F7
-	G7
-	H7
-	A6
-	B6
-	C6
-	D6
-	E6
-	F6
-	G6
-	H6
-	A5
-	B5
-	C5
-	D5
-	E5
-	F5
-	G5
-	H5
-	A4
-	B4
-	C4
-	D4
-	E4
-	F4
-	G4
-	H4
-	A3
-	B3
-	C3
-	D3
-	E3
-	F3
-	G3
-	H3
-	A2
-	B2
-	C2
-	D2
-	E2
-	F2
-	G2
-	H2
-	A1
-	B1
-	C1
-	D1
-	E1
-	F1
-	G1
-	H1
+    A8 BitSquare = iota
+    B8
+    C8
+    D8
+    E8
+    F8
+    G8
+    H8
+    A7
+    B7
+    C7
+    D7
+    E7
+    F7
+    G7
+    H7
+    A6
+    B6
+    C6
+    D6
+    E6
+    F6
+    G6
+    H6
+    A5
+    B5
+    C5
+    D5
+    E5
+    F5
+    G5
+    H5
+    A4
+    B4
+    C4
+    D4
+    E4
+    F4
+    G4
+    H4
+    A3
+    B3
+    C3
+    D3
+    E3
+    F3
+    G3
+    H3
+    A2
+    B2
+    C2
+    D2
+    E2
+    F2
+    G2
+    H2
+    A1
+    B1
+    C1
+    D1
+    E1
+    F1
+    G1
+    H1
 )
 
 // enum for sides of game
 type Side int 
 
 const (
-	White Side = 0 
+	White Side = iota
 	Black 
 )
 var ChessBoard cBoard
@@ -109,14 +109,13 @@ func PrintBitBoard(bitBoard uint64) {
 
 			//sqaure index of the board
 			square := rank*8 + file // 0 - 64
-
+			
 			// empty is 0, filled is 1
 			bit := 0
-			if getBit(bitBoard, square) == 0 {
-				bit = 0
-			} else {
+			if getBit(bitBoard, square) != 0 {
 				bit = 1
-			}
+			} 
+
 			//also print the current rank
 			if file == 0 {
 				fmt.Printf("%d ", 8-rank)
@@ -141,35 +140,138 @@ func getBit(bitBoard uint64, square int) (bit uint64) {
 	// 1 << square will left shift one by 0 - 64
 	// this will be a bitmask at the 0-64th bit
 	// &'ing the bitboard at the bitmask will see if the bit is activated there
-	return bitBoard & (1 << square)
+	return bitBoard & (uint64(1) << uint(square))
 }
 
 // sets the bit at the human readable square to 1
 func SetBit(bitBoard *uint64, square BitSquare) {
-	// 1 << square will left shift one by 0 - 63rd bit
+	// 1 << square will left shift one by 0 - 63rd bit depending on the square
 	// this will create a bitmask at the 0-63rd bit
 	// or'ing the bitboard at the bitmask will set the bit at the square to 1
-	*bitBoard |= (1 << square)
+	*bitBoard |= (uint64(1) << uint(square))
 }
 
 //removes a bit at at the human readable square/ sets it to 0
 func PopBit(bitBoard *uint64, square BitSquare) {
 	if getBit(*bitBoard, int(square)) != 0 { // nonzero value means bit was set there
-		*bitBoard ^= (1 << square) //xor the bit
+		*bitBoard ^=  (uint64(1) << uint(square)) //xor the bit
 	} 
 	// already 0, do nothing
 }
 
-// // generate pawn attacks
-// func mask_pawn_attacks(square BitSquare, side Side) {
-// 	// results of attacks bitboard 
-// 	var attacks uint64 
+// 2d array of uint64
+var pawnAttacks [2][64]uint64
 
-// 	// piece bitboard 
-// 	var bitBoard uint64 
+/*
 
-// 	SetBit(&bitBoard, square)
+	Not A File 
+
+8  0  1  1  1  1  1  1  1 
+7  0  1  1  1  1  1  1  1 
+6  0  1  1  1  1  1  1  1 
+5  0  1  1  1  1  1  1  1 
+4  0  1  1  1  1  1  1  1 
+3  0  1  1  1  1  1  1  1 
+2  0  1  1  1  1  1  1  1 
+1  0  1  1  1  1  1  1  1 
+
+   a  b  c  d  e  f  g  h 
+*/
+const NotAFile uint64 = 18374403900871474942 
+
+/*
+
+	Not H File 
+
+8  1  1  1  1  1  1  1  0 
+7  1  1  1  1  1  1  1  0 
+6  1  1  1  1  1  1  1  0 
+5  1  1  1  1  1  1  1  0 
+4  1  1  1  1  1  1  1  0 
+3  1  1  1  1  1  1  1  0 
+2  1  1  1  1  1  1  1  0 
+1  1  1  1  1  1  1  1  0 
+
+   a  b  c  d  e  f  g  h 
+*/
+	
+const NotHFile uint64 = 9187201950435737471
+
+/*
+
+	Not HG File
+
+8  1  1  1  1  1  1  0  0 
+7  1  1  1  1  1  1  0  0 
+6  1  1  1  1  1  1  0  0 
+5  1  1  1  1  1  1  0  0 
+4  1  1  1  1  1  1  0  0 
+3  1  1  1  1  1  1  0  0 
+2  1  1  1  1  1  1  0  0 
+1  1  1  1  1  1  1  0  0 
+
+   a  b  c  d  e  f  g  h 
+*/
+const NotHGFile uint64 = 4557430888798830399
+
+/*
+	Not AB File
+8  0  0  1  1  1  1  1  1 
+7  0  0  1  1  1  1  1  1 
+6  0  0  1  1  1  1  1  1 
+5  0  0  1  1  1  1  1  1 
+4  0  0  1  1  1  1  1  1 
+3  0  0  1  1  1  1  1  1 
+2  0  0  1  1  1  1  1  1 
+1  0  0  1  1  1  1  1  1 
+
+   a  b  c  d  e  f  g  h 
+*/
+
+const NotABFile uint64 = 18229723555195321596
 
 
+func Mask_pawn_attacks(square BitSquare, side Side) (attacks uint64) {
+	// results of attacks bitboard 
+	// var attacks uint64 
 
-// }
+	// piece bitboard 
+	var bitBoard uint64 
+
+	// set piece on the bitboard 
+	SetBit(&bitBoard, square)
+
+	// generate the attacks from the piece
+	switch side {
+	case White:
+		// white pawn attack on the diagonal top right
+		// if we are on h file, we want to make sure we don't wrap around to A file 
+		if ( (bitBoard >> 7) & NotAFile) != 0 { // top right attack &'d notAfile is 0, then we are trying to wrap around to A File
+			attacks |= (bitBoard >> 7)
+		}	
+
+		// white pawn attack on the diagonal top left
+		// if we are on a file, we want to make sure we don't wrap around to H file 
+		if ( (bitBoard >> 9) & NotHFile) != 0 { // top left attack &'d notHFile is 0, then we are trying to wrap around to H File
+			attacks |= (bitBoard >> 9)
+		}
+		
+	case Black:
+		// black pawn attack on the diagonal bottom right
+		// if we are on a file, we want to make sure we don't wrap around to H file 
+		if ( (bitBoard << 7) & NotHFile) != 0 { // TODO
+			attacks |= (bitBoard << 7)
+		}	
+
+		// black pawn attack on the diagona bottom left
+		// if we are on h file, we want to make sure we don't wrap around to a file 
+		if ( (bitBoard << 9) & NotAFile) != 0 { // TODO
+			attacks |= (bitBoard << 9)
+		}
+	}
+
+	return attacks
+
+
+}
+
--- main.go
+++ main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+
 	"github.com/JamesVPog/chess_engine_in_go/board"
 )
 
 
 
 func main() {
-	board.SetBit(&board.ChessBoard.WhitePawns, board.H8)
-	board.SetBit(&board.ChessBoard.WhitePawns, board.E4)
-	board.SetBit(&board.ChessBoard.WhitePawns, board.D3)
+	// set the e4, d4, and h7 bit
+	// board.PrintBitBoard(board.Mask_pawn_attacks(board.E4, board.White))
 
-	board.PrintBitBoard(board.ChessBoard.WhitePawns)
+	// var hgFile uint64 
+	// hgFile = 0
+	// // generate picture for file in comments
+	// for rank := 0; rank < 8; rank++ {
+	// 	for file := 0; file < 8; file++ {
+	// 		square := rank * 8 + file
+			 
+	// 		if file > 1 { 
+	// 			board.SetBit(&hgFile, board.BitSquare(square))
+	// 		}
+	// 	}
+	// }
 
-	board.PopBit(&board.ChessBoard.WhitePawns, board.E4)
-
-	board.PrintBitBoard(board.ChessBoard.WhitePawns)
-
-	board.PopBit(&board.ChessBoard.WhitePawns, board.E4)
 	
-	board.PrintBitBoard(board.ChessBoard.WhitePawns)
+	// attack mask of the e4 pawn 
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.E4, board.White))
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.E4, board.Black))
 
-	board.PopBit(&board.ChessBoard.WhitePawns, board.E4)
-	
-	board.PrintBitBoard(board.ChessBoard.WhitePawns)
+	// attack mask of edge cases: the h4 pawn
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.H4, board.White))
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.H4, board.Black))
+
+
+	// attack mask of edge cases: the a4 pawn 
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.A4, board.White))
+	board.PrintBitBoard(board.Mask_pawn_attacks(board.A4, board.Black))
 }
